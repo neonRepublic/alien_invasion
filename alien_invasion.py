f5db04ce65231706ffdfe1654c5cb44bea538e69
Shaun <194363997+neonRepublic@users.noreply.github.com>
2025-08-24 19:51:34 -0400

    updated window color on start, clock and framerate

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,9 +6,12 @@ class AlienInvasion:
     def __init__(self):
         """Starts the game and creates game resources"""
         pygame.init()
-
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+
+        #Sets background color
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start main game loop"""
@@ -18,8 +21,12 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # Redraws screen on each pass through of the loop.
+            self.screen.fill(self.bg_color)
+
             #Make most recent screen visible
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__== '__main__':
     # Make a game instane and run game
