27deab792c036de5c6814333bbe7a634f68aa6a9
Shaun <194363997+neonRepublic@users.noreply.github.com>
2025-08-24 20:12:35 -0400

    created settings file then imported to main game file

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,13 +1,18 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Class used to manage assets and gamne behavior"""
     def __init__(self):
         """Starts the game and creates game resources"""
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1200, 800))
+        # self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         #Sets background color
