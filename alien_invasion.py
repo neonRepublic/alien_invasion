ef9350747a90478757f24c80b3c59e38601fe83c
Shaun <194363997+neonRepublic@users.noreply.github.com>
2025-08-25 23:49:33 -0400

    added ability to quit using 'q', made fullscreen

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,14 +11,12 @@ class AlienInvasion:
         """Starts the game and creates game resources"""
         pygame.init()
         self.clock = pygame.time.Clock()
-        # self.screen = pygame.display.set_mode((1200, 800))
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
-
         self.ship = Ship(self)
-
         #Sets background color
         self.bg_color = (230, 230, 230)
 
@@ -47,6 +45,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         """Respond to key releases"""
