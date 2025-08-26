e7fd7644f207e33910569e29734b89d094b19240
Shaun <194363997+neonRepublic@users.noreply.github.com>
2025-08-25 23:34:54 -0400

    updated speed, limited screen range

--- ship.py
+++ ship.py
@@ -6,25 +6,31 @@ class Ship:
     def __init__(self, ai_game):
         """Initializes ship and start point."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Loads ship and rect
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
-
         # Start each new ship at bottom center
         self.rect.midbottom = self.screen_rect.midbottom
+        #store a float for ships exact horizontal positioning
+        self.x = float(self.rect.x)
 
         # Flag - starts with an imobile ship
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
-        """Upodate ships position based on movement flags"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        """Update ships position based on movement flags"""
+        # Update ship's x value, not rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     def bliteme(self):
         """Draw ship at current location"""
