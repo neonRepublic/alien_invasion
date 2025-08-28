6052b636358191177a794ca19b8f0f965a8b446b
Shaun <194363997+neonRepublic@users.noreply.github.com>
2025-08-27 22:10:44 -0400

    refacotred alien fleet code, added rows

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,8 +6,11 @@ from ship import Ship
 from bullet import Bullet
 from alien import Alien
 
+
+
 class AlienInvasion:
     """Class used to manage assets and gamne behavior"""
+
 
     def __init__(self):
         """Starts the game and creates game resources"""
@@ -26,11 +29,33 @@ class AlienInvasion:
         #Sets background color
         self.bg_color = (230, 230, 230)
 
+
     def _create_fleet(self):
         """creates alien fleet"""
-        #make an alien
+        # create alien and keep adding until no room left
+        #spacing between aliens is one alien width, height
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size
+
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+
+            # finish row and reset x value, increment y value
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+
+    def _create_alien(self, x_position, y_position):
+            """create an alien and place it in a row"""
+            new_alien = Alien(self)
+            new_alien.x = x_position
+            new_alien.rect.x = x_position
+            new_alien.rect.y = y_position
+            self.aliens.add(new_alien)
 
 
     def run_game(self):
@@ -42,6 +67,7 @@ class AlienInvasion:
             self._update_screen()
             self.clock.tick(60)
 
+
     def _update_bullets(self):
         self.bullets.update()
         #gets rid of old bullets to conserve memory
@@ -49,6 +75,7 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
         print(len(self.bullets))
+
 
     def _check_events(self):
         """Respond to keypress and mouse"""
@@ -61,6 +88,7 @@ class AlienInvasion:
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
 
+
     def _check_keydown_events(self, event):
         """Respond to keypresses"""
         if event.key == pygame.K_RIGHT:
@@ -72,6 +100,7 @@ class AlienInvasion:
         elif event.key == pygame.K_SPACE:
             self.fire_bullets()
 
+
     def _check_keyup_events(self, event):
         """Respond to key releases"""
         if event.key == pygame.K_RIGHT:
@@ -79,11 +108,13 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+
     def fire_bullets(self):
         """creates bullet and adds to bullets group"""
         if len(self.bullets) < self.settings.bullet_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
 
     def _update_screen(self):
         """Updates images on screen then flips to new screen"""
@@ -97,6 +128,7 @@ class AlienInvasion:
         #Make most recent screen visible
         pygame.display.flip()
             
+
 
 if __name__== '__main__':
     # Make a game instane and run game
